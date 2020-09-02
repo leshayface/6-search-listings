4e7b1268812d108d76bec6c583f7136491af02b0
Leshay Adams <leshayadams@Leshays-MacBook-Pro.local>
2020-09-02 12:16:33 +0200

    feat(SearchForm): add search for title string

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import SearchForm from './search/SearchForm'
 import axios from 'axios';
 
 
@@ -14,19 +15,34 @@ export interface Values {
 
 export default function Home() {
 
+  const [search, setSearch] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+
   const [data, setData] = useState([] as Values[]);
   useEffect(() => {
       getData();
-  }, []);
+  }, [searchValue]);
 
   const getData = async () => {
-    const listings = await axios.get(`http://localhost:5000/listings`);
+    const listings = await axios.get(`http://localhost:5000/listings?title_like=${searchValue}`);
     setData(listings.data);
     console.log(listings)
   }
 
+  const updateSearch = (e: any) => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  const updateSearchValue = (e: any) => {
+    e.preventDefault();
+    setSearchValue(search);
+    search === "" && setSearchValue("salad");
+  }
+
   return (
-    <div className="flex flex-col items-center bg-gray-200">
+    <div className="flex flex-col items-center bg-gray-200 h-screen">
+      <SearchForm search={search} updateSearch={updateSearch} updateSearchValue={updateSearchValue}/>
       {data.map(listing => (
         <div className="max-w-lg bg-white rounded overflow-hidden shadow-lg my-8 w-3/4 text-center" key={listing.title}>
           <div className="px-6 py-4">
